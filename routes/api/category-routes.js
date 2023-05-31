da80b81234725786812ae97980d0e6a9d9946455
XyrillaSC <professionalstevenkramer@gmail.com>
2023-05-31 18:35:24 -0400

    added all api routes and tested

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,13 +3,31 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
+  try {
+    const catData = await Category.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(catData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // be sure to include its associated Products
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
+  try {
+    const catData = await Category.findByPk(req.params.id, {include: [{ model: Product }]});
+    if (!catData) {
+      res.status(404).json({ message: 'No category with this id!' });
+      return;
+    }
+    res.status(200).json(catData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // be sure to include its associated Products
 });
 
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,14 +4,32 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
+  try {
+    const prodData = await Product.findAll({
+      include: [{ model: Tag }, { model: Category }],
+    });
+    res.status(200).json(prodData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // be sure to include its associated Category and Tag data
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
+  try {
+    const prodData = await Product.findByPk(req.params.id, {include: [{ model: Tag }, { model: Category }]});
+    if (!prodData) {
+      res.status(404).json({ message: 'No product with this id!' });
+      return;
+    }
+    res.status(200).json(prodData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // be sure to include its associated Category and Tag data
 });
 
